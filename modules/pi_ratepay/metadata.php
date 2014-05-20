aed0893f447a6c434530d29f0fe70fe1edd37e1c
Volker Rahmlow <volker.rahmlow@ratepay.com>
2014-05-20 10:19:03 +0200

    Update metadata.php

--- modules/pi_ratepay/metadata.php
+++ modules/pi_ratepay/metadata.php
@@ -29,7 +29,7 @@ $aModule = array(
     ),
     'thumbnail'    => 'picture.png',
     'lang'         => 'en',
-    'version'      => '3.1.2',
+    'version'      => '3.1.3',
     'author'       => 'RatePAY GmbH',
     'url'          => 'http://www.ratepay.com/',
     'extend'       => array(
