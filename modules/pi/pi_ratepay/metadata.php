f667a8cfb72bd4d0b1ca973be64d75179a2206fd
Vincent Boulanger <vincent.boulanger@fatchip.de>
2020-03-12 15:30:47 +0100

    OX-32 : Version update to 5.0.6

--- modules/pi/pi_ratepay/metadata.php
+++ modules/pi/pi_ratepay/metadata.php
@@ -33,7 +33,7 @@ $aModule = array(
     ),
     'thumbnail'    => 'ratepay_logo.png',
     'lang'         => 'en',
-    'version'      => '5.0.5',
+    'version'      => '5.0.6',
     'author'       => 'RatePAY GmbH',
     'email'        => 'integration@ratepay.com',
     'url'          => 'http://www.ratepay.com/',
