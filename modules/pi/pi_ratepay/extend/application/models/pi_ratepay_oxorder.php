8bda617090824a9dcc57194d3e6e58358e386c1d
boulangerV <vincent.boulanger.ed@gmail.com>
2020-06-18 16:54:23 +0200

    OX-19: include ELV into the check for ordernr generation when empty

--- modules/pi/pi_ratepay/extend/application/models/pi_ratepay_oxorder.php
+++ modules/pi/pi_ratepay/extend/application/models/pi_ratepay_oxorder.php
@@ -12,7 +12,10 @@ class pi_ratepay_oxorder extends pi_ratepay_oxorder_parent
      */
     protected function _executePayment(oxBasket $oBasket, $oUserpayment)
     {
-        if ($oUserpayment->oxuserpayments__oxpaymentsid->value  == "pi_ratepay_rate" || $oUserpayment->oxuserpayments__oxpaymentsid->value == "pi_ratepay_rechnung") {
+        if ($oUserpayment->oxuserpayments__oxpaymentsid->value  == "pi_ratepay_rate"
+            || $oUserpayment->oxuserpayments__oxpaymentsid->value == "pi_ratepay_rechnung"
+            || $oUserpayment->oxuserpayments__oxpaymentsid->value == "pi_ratepay_elv"
+        ) {
             if (!$this->oxorder__oxordernr->value) {
                 $this->_setNumber();
             } else {
